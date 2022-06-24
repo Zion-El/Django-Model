1230f428b567e9c1e5716111484a95a5672fbbba
Zion-El <mchladelola@gmail.com>
2022-06-24 12:45:22 +0100

    Initial django commit

---
+++ blog/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+# Create your models here.
+class Post(models.Model):
+    Title = models.CharField(max_length=200)
+    Text = models.TextField()
+    Author = get_user_model()
+    Create_date = models.DateTimeField()
+    Published_date = models.DateTimeField()
+
+    def __str__(self) -> str:
+        return self.title 
